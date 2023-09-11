6be09b361f670a90642205ddc93884c0a7947877
PedroHIegler <pedroiegler1601@gmail.com>
2023-09-10 23:31:34 -0300

    feature: nova arquitetura

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function App() {
+
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const loadData = () => {
+      fetch('http://localhost:8000/api/customers/')
+      .then(response => response.json())
+      .then(data => setCustomers(data))
+    }
+    loadData();
+  }, [])
+
+  return (
+    <div className="App">
+      {customers.map(customer => (
+        <h1 key={customer.id}>{customer.first_name} {customer.last_name}</h1>
+      ))}
+    </div>
+  );
+}
+
+export default App;
