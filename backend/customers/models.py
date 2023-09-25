2e347ae1cccecfd77212a9f396d912c6f709da59
PedroHIegler <pedroiegler1601@gmail.com>
2023-09-24 23:18:52 -0300

    feature: desenvolvendo a autenticação

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
@@ -11,3 +12,17 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
     
+class User(AbstractUser):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=8)
+    email = models.EmailField(max_length=254, unique=True, error_messages={'unique': "O email cadastrado já existe."})
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(auto_now=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    groups = models.ManyToManyField('auth.Group', related_name='custom_user_set')
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_permissions')
+    
+    USERNAME_FIELD = 'email'
--- backend/customers/serializers.py
+++ backend/customers/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Customer
+from .models import Customer, User
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ('id', 'first_name', 'last_name', 'date_of_birth',)
         read_only_fields = ('id',)
+        
+class UserSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True,
+        label="Senha"
+    )
