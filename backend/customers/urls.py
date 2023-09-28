890a69e97df9f3a4514065f12cc24c4f74b2e08c
PedroHIegler <pedroiegler1601@gmail.com>
2023-09-27 23:32:02 -0300

    feature: autenticação

--- backend/customers/urls.py
+++ backend/customers/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from django.contrib import admin
 
 from . import views
 
 app_name = 'customers'
 
+router = routers.DefaultRouter()
+router.register('cadastrar_usuario', views.UsersViewSet, basename='Cadastrar Usuário')
+
 urlpatterns = [
     path('customers/', views.CustomerListAPIView.as_view(), name='customer-list'),
     path('customers/create/', views.CustomerCreateAPIView.as_view(), name='customer-create'),
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('cadastrar_usuario/', views.ListUsers.as_view())
 ]
--- backend/customers/views.py
+++ backend/customers/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from rest_framework import mixins, generics, permissions
+from rest_framework import mixins, generics, permissions, viewsets
 
-from .models import Customer
-from .serializers import CustomerSerializer
+from .models import Customer, User
+from .serializers import CustomerSerializer, UserSerializer
 
 class CustomerListAPIView(generics.ListAPIView):
     serializer_class = CustomerSerializer
@@ -15,3 +15,15 @@ class CustomerCreateAPIView(generics.CreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [permissions.AllowAny]
+    
+    
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class ListUsers(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = User.objects.all()
+        return queryset
+    serializer_class = UserSerializer
+    
